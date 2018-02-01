110dcc7003d74c05a4f1db98cc4d98f3350e5909
Rashad Akoodie <rakoodie@retrorabbit.co.za>
2018-02-01 11:03:13 +0200

    Init

---
+++ TestApp/RootViewController.designer.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace TestApp
+{
+    [Register("RootViewController")]
+    partial class RootViewController
+    {
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
